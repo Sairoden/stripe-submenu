0077631399cb051abaa965491950ba4d01660fe6
Sairoden <odingandarosa@gmail.com>
2022-12-24 17:11:04 +0800

    final

--- src/Hero.js
+++ src/Hero.js
@@ -1,8 +1,31 @@
 import React from "react";
 import phoneImg from "./images/phone.svg";
+import { useGlobalContext } from "./context";
 
 const Hero = () => {
-  return <h2>hero component</h2>;
+  const { closeSubmenu } = useGlobalContext();
+
+  return (
+    <section className="hero" onMouseOver={closeSubmenu}>       
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payments infrastructure for the internet</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia
+            vitae culpa esse corrupti cupiditate ex sit magni, repellendus
+            suscipit repudiandae! Officiis, minima atque placeat voluptate
+            dolores omnis ut illo sint. Blanditiis molestiae nam doloribus,
+            adipisci quos aliquid totam nihil quo consequuntur. Quasi adipisci,
+            ea velit optio laborum est! Quasi, harum?
+          </p>
+          <button className="btn">Start now</button>
+        </article>
+        <article className="hero-images">
+          <img className="phone-img" src={phoneImg} alt="phone" />
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
--- src/Submenu.js
+++ src/Submenu.js
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { FaTimes } from "react-icons/fa";
 import sublinks from "./data";
+import { useGlobalContext } from "./context";
 
 const Sidebar = () => {
-  return <h2>sidebar component</h2>;
+  const {
+    isSubmenuOpen,
+    location,
+    page: { page, links },
+  } = useGlobalContext();
+  const container = useRef(null);
+
+  const [columns, setColumns] = useState("col-2");
+
+  useEffect(() => {
+    setColumns("col-2");
+
+    const subMenu = container.current;
+    const { center, bottom } = location;
+
+    subMenu.style.left = `${center}px`;
+    subMenu.style.top = `${bottom}px`;
+
+    if (links.length === 3) setColumns("col-3");
+    else if (links.length > 3);
+
+    setColumns("col-4");
+  }, [links, location]);
+
+  return (
+    <aside ref={container} className={`submenu ${isSubmenuOpen && "show"}`}>
+      <h4>{page}</h4>
+      <div className={`submenu-center ${columns}`}>
+        {links.map((link, index) => {
+          const { label, icon, url } = link;
+          return (
+            <a href={url} key={{ index }}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
+    </aside>
+  );
 };
 
 export default Sidebar;
+
+// 69 KA NA
